4be612cdbf835e9bfefa2bdefadb1d0fde5b1377
nindleMac <1104892323@qq.com>
2023-12-07 19:34:23 +0800

    update: title

--- config/config.ts
+++ config/config.ts
@@ -66,7 +66,7 @@ export default defineConfig({
    * @name layout 插件
    * @doc https://umijs.org/docs/max/layout-menu
    */
-  title: 'Ant Design Pro',
+  title: 'Nindle',
   layout: {
     locale: true,
     ...defaultSettings,
