c157d1abb4ad0fdb7516334561872284fb6b2b33
nindleMac <1104892323@qq.com>
2024-04-16 19:06:41 +0800

    feat: baseURL

--- src/app.tsx
+++ src/app.tsx
@@ -49,5 +49,7 @@ export const layout: RunTimeLayoutConfig = ({ initialState }) => {
  * @doc https://umijs.org/docs/max/request#配置
  */
 export const request = {
+  baseURL:
+    process.env["NODE_ENV"] !== "development" && "https://121.41.117.26:6200",
   ...errorConfig,
 }
